a34d081d032f9de8a6414b28653d1646811ad1e7
doitforme <kotovromanolegovich97@gmail.com>
2023-03-15 20:33:03 +0300

    process upd in separate goroutine

--- bot/run.go
+++ bot/run.go
@@ -20,59 +20,63 @@ func (b *bot) Run() {
 		Timeout: 10,
 	}
 	for upd := range b.GetUpdatesChan(updatesCfg) {
-		if upd.MyChatMember != nil {
-			// if user left or kicked bot
-			if upd.MyChatMember.NewChatMember.Status == "left" || upd.MyChatMember.NewChatMember.Status == "kicked" {
-				b.chatsMu.Lock()
-				delete(b.chats, upd.MyChatMember.Chat.ID)
-				b.chatsMu.Unlock()
-			}
+		go b.processUpdate(upd)
+	}
+}
+
+func (b *bot) processUpdate(upd tgbotapi.Update) {
+	if upd.MyChatMember != nil {
+		// if user left or kicked bot
+		if upd.MyChatMember.NewChatMember.Status == "left" || upd.MyChatMember.NewChatMember.Status == "kicked" {
+			b.chatsMu.Lock()
+			delete(b.chats, upd.MyChatMember.Chat.ID)
+			b.chatsMu.Unlock()
+		}
+	}
+
+	if upd.Message != nil {
+		if !b.chatExist(upd.Message.Chat.ID) {
+			b.addChat(upd.Message.Chat.ID)
 		}
 
-		if upd.Message != nil {
-			if !b.chatExist(upd.Message.Chat.ID) {
-				b.addChat(upd.Message.Chat.ID)
-			}
-
-			if upd.Message.IsCommand() {
-				key := upd.Message.Command()
-				if cmd, ok := b.commands[commandKey(key)]; ok {
-					go cmd.action(upd)
-				} else {
-					b.logger.Error("command handler not found", zap.String("cmd", key))
-				}
-				continue
-			}
-
-			if cmd, ok := b.replyToCommand(upd.Message.Text); ok {
+		if upd.Message.IsCommand() {
+			key := upd.Message.Command()
+			if cmd, ok := b.commands[commandKey(key)]; ok {
 				go cmd.action(upd)
-				continue
-			}
-
-			if strings.HasPrefix(upd.Message.Text, onlinePrefix) {
-				go b.OnlineCmd(upd)
-			} else if strings.HasPrefix(upd.Message.Text, promoPrefix) {
-				go b.PromoCmd(upd)
-			} else if strings.HasPrefix(upd.Message.Text, conversionRatesPrefix) {
-				b.ConversionRates(upd)
 			} else {
-				go b.SearchCmd(upd)
+				b.logger.Error("command handler not found", zap.String("cmd", key))
 			}
+			return
 		}
 
-		if upd.CallbackQuery != nil {
-			data := upd.CallbackData()
-			entity := unmarshallCb(data)
-
-			if entity.cbType != Search && entity.parentType != Search {
-				b.clearSearchParams(upd.CallbackQuery.Message.Chat.ID)
-			}
-
-			callback := tgbotapi.NewCallback(upd.CallbackQuery.ID, "")
-			b.apiRequest(callback)
-
-			b.callbacks[entity.cbType](upd, entity)
+		if cmd, ok := b.replyToCommand(upd.Message.Text); ok {
+			go cmd.action(upd)
+			return
 		}
+
+		if strings.HasPrefix(upd.Message.Text, onlinePrefix) {
+			go b.OnlineCmd(upd)
+		} else if strings.HasPrefix(upd.Message.Text, promoPrefix) {
+			go b.PromoCmd(upd)
+		} else if strings.HasPrefix(upd.Message.Text, conversionRatesPrefix) {
+			b.ConversionRates(upd)
+		} else {
+			go b.SearchCmd(upd)
+		}
+	}
+
+	if upd.CallbackQuery != nil {
+		data := upd.CallbackData()
+		entity := unmarshallCb(data)
+
+		if entity.cbType != Search && entity.parentType != Search {
+			b.clearSearchParams(upd.CallbackQuery.Message.Chat.ID)
+		}
+
+		callback := tgbotapi.NewCallback(upd.CallbackQuery.ID, "")
+		b.apiRequest(callback)
+
+		b.callbacks[entity.cbType](upd, entity)
 	}
 }
 
